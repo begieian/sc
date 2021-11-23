c243f228c002af5a136e624769ab923fa34d0f9f
begieian <begieianrivera@gmail.com>
2021-11-23 13:44:48 +0800

    imported redux persist to persist state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
+import { store, persistor } from './redux/store';
+
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
   <Provider store={ store }>
     <Router>
       <React.StrictMode>
-        <App />
+        <PersistGate persistor={ persistor }>
+          <App />
+        </PersistGate>
       </React.StrictMode>
     </Router>
   </Provider>,
